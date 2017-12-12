5db3c7bef32e8baa545b25b10dec3a278a2e9cd9
vmonacillo <vmonacillo@gmail.com>
2017-12-12 21:02:28 +0800

    Add files via upload

--- guess.php
+++ guess.php
@@ -1,54 +1,50 @@
-<html>
-<head>
-	<title>Guessing Game by Vincent Monacillo</title>
-</head>
-<body>
-	Guessing Game <br>
-
-	by Vincent F. Monacillo <br>
-
 <?php
-$randomnumber= mt_rand(1,5);
-if(isset($_POST['number_entered']))
-{
-	$number=$_POST['number_entered'];
-}
-if(isset($_POST['submit']))
-{
-	$submitbutton=$_POST['submit'];
-}
-?>
+ 
+ session_start();
 
-<form action="" method="POST">
-Guess a Number Between 1 and 5: 
-<input type="text" name="number_entered" value=''/> <br><br>
-
-Result: 
-<?php 
-if (isset($submitbutton)){
-
-	if (($number > 0) && ($number <6))
-	{
-		if ($number != $randomnumber)
-		{
-			if ( $number < $randomnumber)
-				{
-				echo "Incorrect guess. The correct number was higher than the required. The correct number was $randomnumber. Try again?";
-				} 
-				else
-				{
-					echo "Incorrect guess. The correct number was lower than the required. The correct number was $randomnumber. Try again?";
-				}
-		}
-		else 
-		{
-			echo "$randomnumber is the correct guess. You got it right.";
-		}
-	}
-}
-?>
-	<br><br>
-<input type="submit" name="submit" value="Guess"/><br><br>
+ if(!isset($_GET['guess'])){
+	 echo("Please Input Value");
+	  $_SESSION["counter"] = 0;
+ }else if (strlen($_GET['guess'])< 1 ) {
+	 echo("Your Guess is too short"); 
+	 $_SESSION["counter"]++;
+ }else if ( ! is_numeric($_GET['guess'])) {
+	 echo("Your Guess is not a number");
+	 $_SESSION["counter"]++;
+ }else if ($_GET['guess'] < 42) {
+	 echo("Your Guess is too low");
+	 $_SESSION["counter"]++;
+ }else if ($_GET['guess'] > 42) {
+	 echo("Your Guess is too high");
+	 $_SESSION["counter"]++;
+ } else{
+	 echo("Congratulation - Your Right!!!");
+	 unset($_SESSION["counter"]);
+	 session_destroy();
+ }
+ if ($_SESSION["counter"]==5) {
+	echo "You have 5 tries already";
+	echo "<h1>Game is over</h1>";
+	unset($_SESSION["counter"]);
+	session_destroy();
+	echo "<a href='guess1.php'>Play AGain</a>"; 
+	
+ }	 
+ ?>
+ <html>
+ <head>
+ <title>Vincent F. Monacillo</title>
+ </head>
+ <body>
+ <h1>Guessing Game</h1>
+ <h2>by: Vincent F. Monacillo</h2>>
+ <p>
+ <form method="GET">
+<p><label for= "guess">Input Guess</label>
+<input type="text" name="guess" size="40" id="guess"/></p>
+<input type="submit" name="submit"/>
 </form>
-</body>
-</html>
+
+ </p>
+ </body>
+ </html>
