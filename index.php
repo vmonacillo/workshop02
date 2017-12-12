5d1ca14fa47581bda83e4d355c931e44b0fa1587
vmonacillo <vmonacillo@gmail.com>
2017-12-12 21:17:57 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
-<?php
+
+ <html>
+ <head>
+ <title>Vincent F. Monacillo</title>
+ </head>
+ <body>
+ <h1>Guessing Game</h1>
+ <h2>by: Vincent F. Monacillo</h2>
+ <p>
+ <form method="GET">
+<p><label for= "guess">Input Guess</label>
+<input type="text" name="guess" size="40" id="guess"/></p>
+<input type="submit" name="submit"/>
+</form>
+
+ </p>
+ </body>
+ </html>
+ <?php
  
  session_start();
 
- if(!isset($_GET['guess'])){
+ if(! isset($_GET['guess'])){
 	 echo("Please Input Value");
 	  $_SESSION["counter"] = 0;
  }else if (strlen($_GET['guess'])< 1 ) {
@@ -27,24 +45,7 @@
 	echo "<h1>Game is over</h1>";
 	unset($_SESSION["counter"]);
 	session_destroy();
-	echo "<a href='guess1.php'>Play AGain</a>"; 
+	echo "<a href='guess.php'>Play AGain</a>"; 
 	
  }	 
  ?>
- <html>
- <head>
- <title>Vincent F. Monacillo</title>
- </head>
- <body>
- <h1>Guessing Game</h1>
- <h2>by: Vincent F. Monacillo</h2>>
- <p>
- <form method="GET">
-<p><label for= "guess">Input Guess</label>
-<input type="text" name="guess" size="40" id="guess"/></p>
-<input type="submit" name="submit"/>
-</form>
-
- </p>
- </body>
- </html>
